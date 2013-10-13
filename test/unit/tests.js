3775165301925f32dba185278bc8c122ac9be71a
tomassantos <TomasESantos@gmail.com>
2013-10-13 10:58:18 -0700

    Updated test and added findItem.

--- test/unit/tests.js
+++ test/unit/tests.js
@@ -1,72 +1,100 @@
+
 test( "item", function()
 {
-    var expectedUser    =   { 
-                                firstName   :   "John",
-                                lastName    :   "Smith",
-                                userName    :   "john.smith" 
-                            };
-
+    var expectedUser    =   getUsers()[0];
     var user            =   $('.user:first-child').item();
 
     deepEqual(user, expectedUser, 'item user');
 });
-
 test( "items", function()
 {
-    var expectedUsers    =  [ 
-                                {
-                                    firstName   :   "John",
-                                    lastName    :   "Smith",
-                                    userName    :   "john.smith" 
-                                },
-                                {
-                                    firstName   :   "Frank",
-                                    lastName    :   "Hamilton",
-                                    userName    :   "frank.hamilton" 
-                                }
-                            ];
-
-    var users            =  $('.user').items();
+    var expectedUsers   =  getUsers();
+    var users           =  $('.user').items();
 
     deepEqual(users, expectedUsers, 'items users');
 });
 test( "item value", function()
 {
-    var expectedValue    =  'John';
-    var value            =  $('.user:first-child').find('.first-name').itemValue();
+    var expectedUser    =   getUsers()[0];
+    var expectedValue   =   expectedUser.firstName;
+    var value           =   $('.user:first-child').find('.first-name').itemValue();
 
    equal(value, expectedValue, 'items value');
 });
 test( "closest item", function()
 {
-    var expectedUser    =   {
-                                firstName   :   "John",
-                                lastName    :   "Smith",
-                                userName    :   "john.smith"
-                            };
-    var user             =  $('.user:first-child').find('.first-name').closestItem();
+    var expectedUser    =   getUsers()[0];
+    var user            =  $('.user:first-child').find('.first-name').closestItem();
 
     deepEqual(user, expectedUser, 'closest item');
 });
 test( "next item", function()
 {
-    var expectedUser    =   {
-                                firstName   :   "Frank",
-                                lastName    :   "Hamilton",
-                                userName    :   "frank.hamilton"
-                            };
-    var user             =  $('.user:first-child').nextItem();
+    var expectedUser    =   getUsers()[1];
+    var user            =  $('.user:first-child').nextItem();
 
     deepEqual(user, expectedUser, 'next item');
 });
 test( "prev item", function()
 {
-    var expectedUser    =   {
-                                firstName   :   "John",
-                                lastName    :   "Smith",
-                                userName    :   "john.smith"
-                            };
-    var user             =  $('.user:nth-child(2)').prevItem();
+    var expectedUser    =   getUsers()[0];
+    var user            =  $('.user:nth-child(2)').prevItem();
 
     deepEqual(user, expectedUser, 'prev item');
 });
+test( "find item", function()
+{
+    var expectedUser    =   getUsers()[0];
+    var user            =  $('.users').findItem(1);
+
+    deepEqual(user, expectedUser, 'find item');
+});
+
+var getUsers    =   function()
+                    {
+                        var users    =   [ 
+                                                        {
+                                                            firstName   :   "John",
+                                                            lastName    :   "Smith",
+                                                            userName    :   "john.smith",
+                                                            address     :   {
+                                                                                address1: "28 Mary Ann St",
+                                                                                city    : "Newnan",
+                                                                                state   : "GA",
+                                                                                zip     : "30263-2446"
+                                                                            },
+                                                            comments    :   [
+                                                                                {
+                                                                                    text    : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                                                    date    : "12/11/2013",
+                                                                                },
+                                                                                {
+                                                                                    text    : "Suspendisse hendrerit risus sit amet arcu varius consectetur.",
+                                                                                    date    : "12/12/2013",
+                                                                                }
+                                                                            ]
+                                                        },
+                                                        { 
+                                                            firstName   :   "Frank",
+                                                            lastName    :   "Hamilton",
+                                                            userName    :   "frank.hamilton",
+                                                            address     :   {
+                                                                                address1: "49 Chickasaw Bluff Rd",
+                                                                                city    : "Benton",
+                                                                                state   : "KY",
+                                                                                zip     : "42025-0000"
+                                                                            },
+                                                            comments    :   [
+                                                                                {
+                                                                                    text    : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                                                                    date    : "12/11/2013",
+                                                                                },
+                                                                                {
+                                                                                    text    : "Suspendisse hendrerit risus sit amet arcu varius consectetur.",
+                                                                                    date    : "12/12/2013",
+                                                                                }
+                                                                            ]
+                                                        }
+                                                    ];
+                        return users;
+                    };
